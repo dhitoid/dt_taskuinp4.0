4742d76637dcf64d6a19c02b5d7c33fd333de8e1
DT DEV ID <153781142+dhitoid@users.noreply.github.com>
2025-02-19 10:01:26 +0700

    Add files via upload

---
+++ dt_ui/client.lua
@@ -0,0 +1,32 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+function Show(title, content)
+    SendNUIMessage({
+        action = "open",
+        title = title,
+        content = content
+    })
+end
+
+function Close()
+    SendNUIMessage({
+        action = "close",
+    })
+end
+
+exports("Show", Show)
+exports("Close", Close)
+
+
+RegisterCommand('open', function(source, args, rawCommand)
+    if #args < 2 then
+        Show(args[1])
+        return
+    end
+    Show(args[1], args[2])
+end)
+
+
+RegisterCommand('close', function(source, args, RawCommand)
+    Close()
+end)
---
+++ dt_ui/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Dhito'
+description 'DT SCRIPT ID UI System 4.0'
+version '1.0.0'
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/*',
+}
+
+client_script 'client.lua'
